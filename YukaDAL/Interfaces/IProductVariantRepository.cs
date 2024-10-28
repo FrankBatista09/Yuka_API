e69e26a1f6b71c1a6174bc83624c69e4e679a5cc
Eric Mota <ericmota268d@gmail.com>
2024-10-27 21:20:53 -0400

    Add specific product retrieval methods with documentation
    Removed the `GetByBrandAndColorAsync` method and introduced new methods: `GetBySpecificAsync` and `GetByBrandIdAsync`. These new methods provide more granular retrieval of product variants by brand, color, and product type. All new methods include XML documentation comments for better context and understanding.

--- YukaDAL/Interfaces/IProductVariantRepository.cs
+++ YukaDAL/Interfaces/IProductVariantRepository.cs
@@ -5,7 +5,29 @@ namespace YukaDAL.Interfaces
 {
     public interface IProductVariantRepository : IBaseRepository<ProductVariant>
     {
-        Task<List<ProductVariant>> GetByBrandAndColorAsync(int brandId, int colorId);
+        /// <summary>
+        /// Gets a list of product variants by brand and color
+        /// </summary>
+        /// <param name="brandId"></param>
+        /// <param name="colorId"></param>
+        /// <returns>List of ProductVariant</returns>
+        Task<List<ProductVariant>> GetBySpecificAsync(int brandId, int colorId);
+
+        /// <summary>
+        /// Gets a list of product variants by brand
+        /// </summary>
+        /// <param name="brandId"></param>
+        /// <returns></returns>
+        Task<List<ProductVariant>> GetByBrandIdAsync(int brandId);
+
+        /// <summary>
+        /// Gets a list of product variants by brand, color and product type
+        /// </summary>
+        /// <param name="brandId"></param>
+        /// <param name="colorId"></param>
+        /// <param name="productId"></param>
+        /// <returns>List of ProductVariant</returns>
+        Task<List<ProductVariant>> GetBySpecificAsync(int brandId, int colorId, int productId);
 
     }
 }
