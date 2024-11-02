f73854576aee30c54423e9979f4f4ee20156893e
Eric Mota <ericmanuel1807@gmail.com>
2024-11-02 17:53:59 -0400

    Add DTOs, ServiceResult, and update interfaces
    Added new DTO classes (`BrandDto`, `CategoryDto`, `ColorDto`, `ProductDto`, `ProductVariantDto`, `SizeDto`) under `YukaBLL.Core.DtosBase`. Modified `IBaseService<T>` to extend from `ServiceResult` and added `GetAllAsync` and `GetByIdAsync` methods. Introduced `ServiceResult` class in `YukaBLL.Core` with `Success`, `Message`, and `Data` properties. Updated `IProductVariantRepository` in `YukaDAL.Interfaces` to include `GetQuantityAsync` method.

--- YukaBLL/Core/IBaseService.cs
+++ YukaBLL/Core/IBaseService.cs
@@ -1,7 +1,18 @@
 ﻿namespace YukaBLL.Core
 {
-    public interface IBaseService<T> where T : class
+    public interface IBaseService<T> where T : ServiceResult
     {
-        
+        /// <summary>
+        /// Generic Method to get all entities
+        /// </summary>
+        /// <returns>A ServiceResult including a List of the entity</returns>
+        Task<T> GetAllAsync();
+
+        /// <summary>
+        /// Generic Method to get x entity by its id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>A ServiceResult with The entity found</returns>
+        Task<T> GetByIdAsync(int id);
     }
 }
---
+++ YukaBLL/Core/ServiceResult.cs
@@ -0,0 +1,11 @@
+﻿namespace YukaBLL.Core
+{
+    public class ServiceResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public dynamic Data { get; set; }
+
+        public ServiceResult() => Success = true;
+    }
+}
