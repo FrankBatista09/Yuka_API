d6d735aafc868529e691a4de718779ec9213bef6
Eric Mota <ericmanuel1807@gmail.com>
2024-11-04 19:18:10 -0400

    Add new service methods and properties for DTOs
    Added using directives for `YukaBLL.Dtos` and `YukaBLL.Responses` in multiple service interfaces. Introduced new methods in `IBrandService`, `ICategoryService`, `IColorService`, `IProductService`, `IProductVariantService`, `ISizeCategoryService`, and `ISizeService` for adding, updating, and deleting entities. Added bulk operations and stock management methods in `IProductVariantService`. Added `CreatedBy` property in `AddDto` and `ColorName` property in `AddColorDto`.

--- YukaBLL/Contracts/ICategoryService.cs
+++ YukaBLL/Contracts/ICategoryService.cs
@@ -1,9 +1,30 @@
 ﻿using YukaBLL.Core;
+using YukaBLL.Dtos.Category;
 using YukaBLL.Responses.Category;
 
 namespace YukaBLL.Contracts
 {
     public interface ICategoryService : IBaseService<CategoryResponse>
     {
+        /// <summary>
+        /// Adds a new category of sizes to the database.
+        /// </summary>
+        /// <param name="addCategoryDto">The new category to add in the database.</param>
+        /// <returns></returns>
+        Task<CategoryAddResponse> AddCategoryAsync(AddCategoryDto addCategoryDto);
+
+        /// <summary>
+        /// Updates the name of a category of sizes
+        /// </summary>
+        /// <param name="updateCategoryDto">The new name of the category.</param>
+        /// <returns></returns>
+        Task<CategoryUpdateResponse> UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+
+        /// <summary>
+        /// Deletes a category of sizes from the database.
+        /// </summary>
+        /// <param name="deleteCategoryDto">The id of the category to be deleted.</param>
+        /// <returns></returns>
+        Task<CategoryDeleteResponse> DeleteCategoryAsync(DeleteCategoryDto deleteCategoryDto);
     }
 }
--- YukaBLL/Contracts/ISizeService.cs
+++ YukaBLL/Contracts/ISizeService.cs
@@ -1,9 +1,30 @@
 ﻿using YukaBLL.Core;
+using YukaBLL.Dtos.Size;
 using YukaBLL.Responses.Size;
 
 namespace YukaBLL.Contracts
 {
     public interface ISizeService : IBaseService<SizeResponse>
     {
+        /// <summary>
+        /// Add a new size to the database.
+        /// </summary>
+        /// <param name="addSizeDto">New size to add.</param>
+        /// <returns></returns>
+        Task<SizeAddResponse> AddSizeAsync(AddSizeDto addSizeDto);
+
+        /// <summary>
+        /// Updates the size name.
+        /// </summary>
+        /// <param name="updateSizeDto">New name for the size.</param>
+        /// <returns></returns>
+        Task<SizeUpdateResponse> UpdateSizeAsync(UpdateSizeDto updateSizeDto);
+
+        /// <summary>
+        /// Deletes a size from the database.
+        /// </summary>
+        /// <param name="deleteSizeDto">Id of the size.</param>
+        /// <returns></returns>
+        Task<SizeDeleteResponse> DeleteSizeAsync(DeleteSizeDto deleteSizeDto);
     }
 }
