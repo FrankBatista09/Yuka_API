10f6badb94ac842e7f9975d2a10c3a89539fff55
Eric Mota <ericmanuel1807@gmail.com>
2024-11-04 15:12:14 -0400

    Add new service interfaces and update SizeResponse class
    Introduced new service interfaces in YukaBLL.Contracts namespace:
    - IBrandService with BrandResponse
    - ICategoryService with CategoryResponse
    - IColorService with ColorResponse
    - IProductService with ProductResponse
    - IProductVariantService with ProductVariantResponse
    - ISizeCategoryService with SizeCategoryResponse
    - ISizeService with SizeResponse
    
    Modified SizeResponse class to inherit from ServiceResult.
    
    Updated YukaBLL.csproj to exclude Contracts folder.

---
+++ YukaBLL/Contracts/IProductVariantService.cs
@@ -0,0 +1,9 @@
+﻿using YukaBLL.Core;
+using YukaBLL.Responses.ProductVariant;
+
+namespace YukaBLL.Contracts
+{
+    public interface IProductVariantService : IBaseService<ProductVariantResponse>
+    {
+    }
+}
