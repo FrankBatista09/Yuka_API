851d3d825975797b8848e35a1fba44cafa375f1c
Eric Mota <ericmanuel1807@gmail.com>
2024-11-04 15:37:41 -0400

    Add CategoryNameExistsException; update YukaBLL.csproj
    Added a new exception class `CategoryNameExistsException` in the
    `YukaBLL.Exceptions` namespace to handle cases where a category
    name already exists. This enhances error handling and code
    clarity. Updated `YukaBLL.csproj` to remove explicit inclusion
    of the `Exceptions` folder, indicating a change in project
    structure management.

---
+++ YukaBLL/Exceptions/CategoryNameExistsException.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace YukaBLL.Exceptions
+{
+    public class CategoryNameExistsException : Exception
+    {
+        public CategoryNameExistsException(string categoryName) : 
+            base($"The category {categoryName} already exists, try a different name")
+        {
+        }
+    }
+}
