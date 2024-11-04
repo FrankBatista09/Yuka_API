73be9be7d80109b5cddd19a4ff39e122c1f8f946
Eric Mota <ericmanuel1807@gmail.com>
2024-11-04 16:31:53 -0400

    Add new exceptions and improve readability
    Added `PriceBelowEqualsZeroException` and `PurchaseBelowEqualsZeroException` in `YukaBLL.Exceptions.ProductVariant` namespace to handle invalid price and purchase amounts. Improved readability of `SizeCategoryExistsException` by splitting a long string into two lines.

--- YukaBLL/Exceptions/SizeCategory/SizeCategoryExistsException.cs
+++ YukaBLL/Exceptions/SizeCategory/SizeCategoryExistsException.cs
@@ -20,7 +20,8 @@
         /// <param name="sizeId">The id of the size to add to the category</param>
         /// <param name="categoryId">The id of the category where the size is to be added</param>
         public SizeCategoryExistsException(int sizeId, int categoryId) :
-            base($"The size with ID {sizeId} already belongs to the category with ID {categoryId}. Please try a different category.")
+            base($"The size with ID {sizeId} already belongs to the category with ID {categoryId}. " +
+                $"Please try a different category.")
         {
             SizeId = sizeId;
             CategoryId = categoryId;
