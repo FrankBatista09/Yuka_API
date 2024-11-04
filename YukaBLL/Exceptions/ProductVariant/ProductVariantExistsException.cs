4278c3a844a849f9e909833e4816bf8521310b92
Eric Mota <ericmanuel1807@gmail.com>
2024-11-04 16:00:54 -0400

    Add specific entity name exists exceptions
    Added new exception classes for handling cases where an entity with a given name already exists in the database. These exceptions include `BrandNameExistsException`, `CategoryNameExistsException`, `ColorNameExistsException`, `ProductNameExistsException`, and `ProductVariantExistsException`. Each class includes a summary comment, a property for the entity name or ID, and a constructor for initializing the exception with a specific message.
    
    Replaced the old `CategoryNameExistsException` with a new implementation that includes additional documentation and a property for the category name.
    
    Updated `YukaBLL.csproj` to include new folders for `Exceptions\SizeCategory` and `Exceptions\Size`.

---
+++ YukaBLL/Exceptions/ProductVariant/ProductVariantExistsException.cs
@@ -0,0 +1,42 @@
+﻿namespace YukaBLL.Exceptions.ProductVariant
+{
+    /// <summary>
+    /// Exception thrown when a product variant already exists in the database.
+    /// </summary>
+    public class ProductVariantExistsException : Exception
+    {
+        /// <summary>
+        /// Gets the product ID of the product variant that caused the exception.
+        /// </summary>
+        public int ProductId { get; }
+        /// <summary>
+        /// Gets the brand ID of the product variant that caused the exception.
+        /// </summary>
+        public int BrandId { get; }
+        /// <summary>
+        /// Gets the color ID of the product variant that caused the exception.
+        /// </summary>
+        public int ColorId { get; }
+        /// <summary>
+        /// Gets the size ID of the product variant that caused the exception.
+        /// </summary>
+        public int SizeId { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductVariantExistsException"/> class.
+        /// </summary>
+        /// <param name="productId">Product ID.</param>
+        /// <param name="brandId">Brand ID.</param>
+        /// <param name="colorId">Color ID.</param>
+        /// <param name="sizeId">Size ID.</param>
+        public ProductVariantExistsException(int productId, int brandId, int colorId, int sizeId) :
+            base($"The product variant with product ID {productId}, brand ID {brandId}, " +
+                 $"color ID {colorId}, and size ID {sizeId} already exists. Please try a different variant.")
+        {
+            ProductId = productId;
+            BrandId = brandId;
+            ColorId = colorId;
+            SizeId = sizeId;
+        }
+    }
+}
