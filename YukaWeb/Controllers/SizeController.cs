2385aae81cf036bdae5f06edd6c10b06f1b77f56
Eric Mota <ericmanuel1807@gmail.com>
2024-11-12 17:12:41 -0400

    Refactor ColorService, add JWT auth, and new SizeController
    Refactored ColorService to use IColorRepository interface for better flexibility and testability. Added SizeController with endpoints for size retrieval and JWT token handling. Updated Program.cs to configure JWT authentication and Swagger integration. Introduced TokenHandler for JWT token generation. Updated YukaWeb.csproj to include JWT package and removed unnecessary ItemGroup.

---
+++ YukaWeb/Controllers/SizeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YukaBLL.Contracts;
+using YukaWeb.TokenEmissor;
+
+namespace YukaWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SizeController : ControllerBase
+    {
+        private readonly ISizeService _sizeService;
+        private readonly IConfiguration _configuration;
+        public SizeController(ISizeService sizeService, IConfiguration configuration)
+        {
+            _sizeService = sizeService;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            // Obtener los tamaños desde el servicio
+            var sizes = await _sizeService.GetAllAsync();
+
+            var tokenHandler = new TokenHandler(_configuration);
+            var token = tokenHandler.GenerateToken("1", "Juan", "juanklk@gmail.com", "Admin");
+
+            // Retornar ambos en un objeto anónimo
+            return Ok(new
+            {
+                Sizes = sizes,
+                Token = token
+            });
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            
+            return Ok("Data for admin only");
+        }
+
+    }
+}
--- YukaWeb/Program.cs
+++ YukaWeb/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
 using YukaBLL.Contracts;
 using YukaBLL.Services;
 using YukaDAL.Context;
@@ -29,7 +33,40 @@ builder.Services.AddScoped<ISizeCategoryService, SizeCategoryService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Yuka API",
+        Version = "v1"
+    });
+
+    // Configurar el esquema de seguridad para JWT
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Ingrese 'Bearer' [espacio] y luego el token JWT."
+    });
+
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 
 builder.Services.AddDbContext<YukaContext>(options =>
@@ -43,7 +80,29 @@ builder.Services.AddDbContext<YukaContext>(options =>
         }
         ));
 
-builder.Services.AddAuthentication();
+var key = Encoding.ASCII.GetBytes(builder.Configuration["JWT:Key"]);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"], 
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:Audience"], 
+        ValidateLifetime = true,
+        RoleClaimType = ClaimTypes.Role
+    };
+});
+
 builder.Services.AddAuthorization();
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<YukaContext>()
@@ -61,6 +120,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapIdentityApi<ApplicationUser>();  
 app.MapControllers();
