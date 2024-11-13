2f93c69daedc78a9cd4f59df17bf096eecc5177f
Juan Luis Almonte <juanluis.93.of@gmail.com>
2024-11-12 21:26:12 -0400

    Added new properties to ProductVariantDto: BrandName, ProductName, SizeName, and ColorName
    This commit enhances ProductVariantDto by introducing four new properties—BrandName, ProductName, SizeName, and ColorName—to improve the depth of information provided for each product variant. These properties enrich the DTO’s data model, enabling more detailed information display and usage.
    
    Refactored the getAll query to include necessary joins for these properties, allowing for efficient and accurate retrieval of the additional data points without redundant queries. Also refined the getById method to fetch specific records for each new property, ensuring that BrandName, ProductName, SizeName, and ColorName are correctly assigned within ProductVariantDto.
    
    These changes provide a more comprehensive data structure in ProductVariantDto, facilitating more informative and user-friendly output in related application views.

--- YukaBLL/Dtos/ProductVariant/ProductVariantDto.cs
+++ YukaBLL/Dtos/ProductVariant/ProductVariantDto.cs
@@ -4,10 +4,15 @@
     {
         public int VariantId { get; set; }
         public int ProductId { get; set; }
+        public string ProductName { get; set; }
         public int BrandId { get; set; }
+        public string BrandName { get; set; }
         public int SizeId { get; set; }
+        public string SizeName { get; set; }
         public int ColorId { get; set; }
+        public string ColorName { get; set; }
         public int Quantity { get; set; }
         public double Price { get; set; }
+        
     }
 }
